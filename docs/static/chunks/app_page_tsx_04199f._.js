46463cd5787ce00f86d8d463a57b915ec6fa553e
gorkulus <gorkulusmusic@gmail.com>
2025-01-11 21:01:32 -0700

    Update metadata title and description

---
+++ docs/static/chunks/app_page_tsx_04199f._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_tsx_04199f._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_tsx_04199f._.js",
+  "chunks": [
+    "static/chunks/[root of the server]__ef9079._.js",
+    "static/chunks/[next]_internal_font_google_raleway_a09c64b7_module_d5317a.css"
+  ],
+  "source": "dynamic"
+});
