e8c02454c6b624d17b25926f272ab15302e7439f
gorkulus <gorkulusmusic@gmail.com>
2025-01-11 19:58:55 -0700

    Remove GitHub Pages specific config for Netlify deployment

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/apesonape-web' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/apesonape-web/' : '',
   trailingSlash: true,
   reactStrictMode: true,
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
